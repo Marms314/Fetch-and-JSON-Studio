36f1c4d84084ef0355b16db2bc7265b68ddb8df5
Marms <maryarmstrong314@gmail.com>
2020-03-19 20:04:05 -0500

    Added script to add new HTML for each astronaut called with fetch

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            function createAstronaut(astronautObject) {
+                let container = document.getElementById("container");
+                container.insertAdjacentHTML("beforeend",
+                    `<div class="astronaut">
+                        <div class="bio">
+                            <h3>${astronautObject.firstName} ${astronautObject.lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${astronautObject.hoursInSpace}</li>
+                                <li>Active: ${astronautObject.active}</li>
+                                <li>Skills: ${astronautObject.skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src=${astronautObject.picture}>
+                    </div>`
+
+
+                )
+            }
+            for (let i = 0; i < json.length; i++) {
+                createAstronaut(json[i]);
+            };
+        });
+    });
+ });
