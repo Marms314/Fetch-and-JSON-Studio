80e0b31c2d29d93b0652b56eeba0264b66bc3ba5
Marms <maryarmstrong314@gmail.com>
2020-03-19 20:24:32 -0500

    refactored the skills

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ window.addEventListener("load", function() {
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(json) {
             function createAstronaut(astronautObject) {
-                let skills = astronautObject.skills[0];
-                for (let i = 1; i < astronautObject.skills.length; i++) {
-                    skills += ", " + astronautObject.skills[i];
-                };
-
                 let container = document.getElementById("container");
                 container.insertAdjacentHTML("beforeend",
                     `<div class="astronaut">
@@ -15,7 +10,7 @@ window.addEventListener("load", function() {
                             <ul>
                                 <li>Hours in space: ${astronautObject.hoursInSpace}</li>
                                 <li>Active: ${astronautObject.active}</li>
-                                <li>Skills: ${skills}</li>
+                                <li>Skills: ${astronautObject.skills.join(', ')}</li>
                             </ul>
                         </div>
                         <img class="avatar" src=${astronautObject.picture}>
